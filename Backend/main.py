bad309848dd3b98ac9ab18a9e908c8e0d869ab62
Dattaguru Chettiar <gurudatta229028@gmail.com>
2026-04-08 12:44:43 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -44,7 +44,7 @@ def get_db():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="220928",
+        password="YOUR PASSWORD",
         database="contro_app"
     )
 
